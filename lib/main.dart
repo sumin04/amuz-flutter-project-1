4029ba01ccf0e1e00a655ef38a658d215d06b45d
sumin04 <gimsumin259@gmail.com>
2022-12-02 21:02:13 +0900

    Router-Data Web Setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,91 @@
 // import 'dart:js';
 
+// ignore_for_file: library_private_types_in_public_api, avoid_unnecessary_containers
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:go_router/go_router.dart';
 
 void main() {
   return runApp(App());
 }
+// Future<Post>
+Future<List> fetchPost(List<dynamic> list) async {
+   List<dynamic> test = [];
+   // List<dynamic> test2 = [];
+  final response =
+    await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
 
-class App extends StatelessWidget {
+
+  if (response.statusCode == 200) {
+    var datas = json.decode(response.body);
+    log("data : ${datas.toString()}");
+    log("data type : ${datas.runtimeType}");
+
+    print("data size : ${datas.length}");
+
+    for ( var i = 0; i < datas.length; i++){
+      list.add(datas[i]);
+      // test.add(datas[i]);
+    }
+    // test2.add(test[0].split(','));
+    // test2.add(test[0].split(''));
+    // print(test2);
+
+    return test;
+
+
+    // print(Post.fromJson(json.decode(response.body)));
+    // print(json.decode(response.body));
+    // return json.decode(response.body);
+  } else {
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+// void main() => runApp(App());
+
+class Apps extends StatefulWidget {
+  Apps({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class App extends StatelessWidget  {
   App({Key? key}) : super(key: key);
 
-  static const String title = '[ Amuz - project ] User List Page';
+  static const String title = '[ Amuz - project ] Home Page';
 
   @override
   Widget build(BuildContext context) => MaterialApp.router(
@@ -22,56 +97,40 @@ class App extends StatelessWidget {
 
   final GoRouter _router = GoRouter(
     errorBuilder: (context, state) => ErrorScreen(error:state.error),
-      routes: <GoRoute>[
+    routes: <GoRoute>[
 
-        GoRoute(
-            routes: <GoRoute>[
-              GoRoute(
-                path: 'page2',
-                builder: (BuildContext context, GoRouterState state) =>
-                const Page2Screen(),
-              ),
-              // GoRoute(
-              //   path: 'page3',
-              //   builder: (BuildContext context, GoRouterState state) =>
-              //   const Page3Screen(),
-              // ),
-              // GoRoute(
-              //   path: 'page4',
-              //   builder: (BuildContext context, GoRouterState state) =>
-              //   const Page4Screen(),
-              // ),
-            ],
-            path: '/',
-              builder: (BuildContext context, GoRouterState state) =>
-              const Page1Screen(),
-        ),
+      GoRoute(
+        routes: <GoRoute>[
+          GoRoute(
+            path: 'page2',
+            builder: (BuildContext context, GoRouterState state) =>  Apps(),
+          ),
+        ],
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+        const Page1Screen(),
+      ),
     ],
 
   );
+
 }
 
 // the screen of the first page
 class Page1Screen extends StatelessWidget {
-  // Creates a [Page1Screen]
   const Page1Screen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(title: const Text(App.title)),
     body: Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           ElevatedButton(
             onPressed: () => context.go('/page2'),
-            child: const Text('User Post Page'),
+            child: const Text('User List Page'),
           ),
           const SizedBox(height: 10,),
-          // ElevatedButton(
-          //   onPressed: () => context.go('/page3'),
-          //   child: const Text('Go to page 3'),
-          // ),
         ],
       ),
     ),
@@ -79,69 +138,107 @@ class Page1Screen extends StatelessWidget {
 }
 
 // the screen of the 2 page
-class Page2Screen extends StatelessWidget {
-  // Creates a [Page1Screen]
-  const Page2Screen({Key? key}) : super(key: key);
+
+class _MyAppState extends State<Apps> {
+  late Future<List> test;
+  List<dynamic> list = [];
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text('User Post Page')),
-    body: Center(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-        ElevatedButton(
-            onPressed: () => context.go('/'),
-            child: const Text('Go Home')),
-        ],
-      ),
-    ),
-  );
-}
-
-// the screen of the 3 page
-class Page3Screen extends StatelessWidget {
-  // Creates a [Page1Screen]
-  const Page3Screen({Key? key}) : super(key: key);
+  void initState(){
+    test = fetchPost(list);
+    super.initState();
+  }
+  // super.initState();
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text(App.title)),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-              onPressed: () => context.go('/'),
-              child: const Text('Go To Home Page'))
-        ],
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    // throw UnimplementedError();
+    return MaterialApp(
+      // title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
       ),
-    ),
-  );
-}
 
-// the screen of the 4 page
-// class Page4Screen extends StatelessWidget {
-//   // Creates a [Page2Screen]
-//   const Page4Screen({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final router = GoRouter.of(context);
-//     return Scaffold(
-//       appBar: AppBar(title: Text(router.location.toString())),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             ElevatedButton(
-//                 onPressed: () => context.go('/'),
-//                 child: const Text('Go Home'))
-//         ],
-//       ),
-//     ),
-//   );}
-// }
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text('User List Page')
+        ),
+        body: FutureBuilder (
+          future: test,
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            if(snapshot.hasData == false){
+              return CircularProgressIndicator();
+            } else if(snapshot.hasError){
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Error: ${snapshot.error}'
+                ),
+              );
+            }
+            else{
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemCount: list.length,
+                    itemBuilder: (context, index) {
+                      final document = list[index];
+
+                      return Row(
+                        children: [
+                          Container(
+                            child: Text(document["userId"].toString()),
+                          ),
+                          Container(
+                            child: Text(document["id"].toString()),
+                          ),
+                          Container(
+                            child: Text(document["title"]),
+                          ),
+                          Container(
+                            child: Text(document["body"]),
+                          )
+                        ],
+                        // child: Center(
+                        //   child: Column(
+                        //     children: <Widget>[
+                        //       ElevatedButton(
+                        //           onPressed: () => context.go('/'),
+                        //           style: ElevatedButton.styleFrom(
+                        //             primary: Colors.grey,
+                        //           ),
+                        //           child: const Text('Go Home')),
+                        //     ],
+                        //   ),
+                        // ),
+                      );
+                    }
+                )
+                // child : Text(snapshot.data.toString()),
+              );
+            }
+          }
+        ),
+
+        // Center(
+        //   child: Column(
+        //     children: <Widget>[
+        //       ElevatedButton(
+        //           onPressed: () => context.go('/'),
+        //           style: ElevatedButton.styleFrom(
+        //             primary: Colors.grey,
+        //
+        //           ),
+        //           child: const Text('Go Home')),
+        //     ],
+        //   ),
+        // ),
+      ),
+      // ),
+    );
+  }
+}
 
 // error
 class ErrorScreen extends StatelessWidget {
@@ -157,7 +254,7 @@ class ErrorScreen extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          error.toString()
+            error.toString()
         ),
       ),
     );
